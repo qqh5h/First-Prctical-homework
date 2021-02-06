c825899c747c202e77d4d242bc18453db9a80624
ACER <ACER@DESKTOP-P4DU9JL>
2021-02-06 13:01:23 +0300

    First commit

---
+++ First practical homework/src/PrintMyName.java
@@ -0,0 +1,10 @@
+//This porgram is written by my name is waleed
+public class PrintMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("My Name Is Waleed");
+      
+	}
+
+}
